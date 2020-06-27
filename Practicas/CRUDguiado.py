b4260607df4620f4094e5c938164aa8c14efd2e6
Chino <matiastu-te_@hotmail.com>
2020-06-27 14:07:28 -0300

    CRUD completado

--- Practicas/CRUDguiado.py
+++ Practicas/CRUDguiado.py
@@ -4,23 +4,102 @@ import sqlite3
 
 root = Tk()
 
+# ------------ FUNCIONES ------------ 
+
+def conexionBBDD():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+    try:
+        miCursor.execute('''
+            CREATE TABLE DATOSUSUARIOS (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NOMBRE_USUARIO VARCHAR(50),
+            PASSWORD VARCHAR(50),
+            APELLIDO VARCHAR(50),
+            DIRECCION VARCHAR(50),
+            COMENTARIOS VARCHAR(100))
+            ''')
+        messagebox.showinfo("BBDD","La BBDD fue creada exitósamente")
+    
+    except:
+        messagebox.showwarning("¡Atención!","La BBDD ya existe")
+
+def salirApp():
+    valor = messagebox.askquestion("Salir","Jodeme que querés salir")
+    if valor == "yes":
+        root.destroy()
+
+def clearCampos():
+    miID.set("")
+    miNombre.set("")
+    miApellido.set("")
+    miDireccion.set("")
+    miPassword.set("")
+    textoComentario.delete(1.0, END)
+
+def crear():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+    datosAlmacenar = (miNombre.get(), miPassword.get(), miApellido.get(), miDireccion.get(), textoComentario.get(1.0,END))
+    miCursor.execute("insert into DATOSUSUARIOS VALUES (Null,?,?,?,?,?)", datosAlmacenar)
+    miConexion.commit()
+
+    messagebox.showinfo("BBDD","Registro ingresado exitosamente")
+
+def leer():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+    
+    miCursor.execute("select * from DATOSUSUARIOS where ID=" + miID.get())
+    elUsuario = miCursor.fetchall()
+    clearCampos()
+    for i in elUsuario:
+        miID.set(i[0])
+        miNombre.set(i[1])
+        miPassword.set(i[2])
+        miApellido.set(i[3])
+        miDireccion.set(i[4])
+        textoComentario.insert(1.0, i[5])
+
+    miConexion.commit()
+
+def actualizar():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+    
+    #miCursor.execute("update DATOSUSUARIOS set NOMBRE_USUARIO='" + miNombre.get() + "', PASSWORD='" + miPassword.get()+ "', APELLIDO='"+ miApellido.get() + "', DIRECCION='"+miDireccion.get() + "',COMENTARIOS='"+ textoComentario.get("1.0",END)+"' WHERE ID=" + miID.get())
+    datosAlmacenar = (miNombre.get(), miPassword.get(), miApellido.get(), miDireccion.get(), textoComentario.get(1.0,END))
+    miCursor.execute("update DATOSUSUARIOS set NOMBRE_USUARIO=?, PASSWORD=?, APELLIDO=?, DIRECCION=?, COMENTARIOS=?" + "where ID=" + miID.get(), datosAlmacenar)
+    miConexion.commit()
+
+    messagebox.showinfo("BBDD","Registro actualizado exitosamente")
+
+def eliminar():
+    miConexion = sqlite3.connect("Usuarios")
+    miCursor = miConexion.cursor()
+
+    miCursor.execute("delete from DATOSUSUARIOS where ID=" + miID.get())
+    miConexion.commit()
+
+    messagebox.showinfo("BBDD","Registro eliminado exitosamente")
+
 # ------------ MENUS ------------ 
 
 barraMenu = Menu(root)
 root.config(menu = barraMenu, width = 300, height = 300)
 
 menuBBDD = Menu(barraMenu, tearoff = 0)
-menuBBDD.add_command(label = "Conectar")
-menuBBDD.add_command(label = "Salir")
+menuBBDD.add_command(label = "Conectar", command = conexionBBDD)
+menuBBDD.add_command(label = "Salir", command = salirApp)
 
 menuBorrar = Menu(barraMenu, tearoff = 0)
-menuBorrar.add_command(label = "Borrar todo")
+menuBorrar.add_command(label = "Borrar todo", command = clearCampos)
 
 menuCRUD = Menu(barraMenu, tearoff = 0)
-menuCRUD.add_command(label = "Crear")
-menuCRUD.add_command(label = "Leer")
-menuCRUD.add_command(label = "Actualizar")
-menuCRUD.add_command(label = "Borrar")
+menuCRUD.add_command(label = "Crear", command = crear)
+menuCRUD.add_command(label = "Leer", command = leer)
+menuCRUD.add_command(label = "Actualizar", command = actualizar)
+menuCRUD.add_command(label = "Borrar", command = eliminar)
 
 menuAyuda = Menu(barraMenu, tearoff = 0)
 menuAyuda.add_command(label = "Licencia")
@@ -36,21 +115,31 @@ barraMenu.add_cascade(label = "Ayuda", menu = menuAyuda)
 miFrame = Frame(root)
 miFrame.pack()
 
-cuadroID = Entry(miFrame)
+miID = StringVar()
+
+cuadroID = Entry(miFrame, textvariable = miID)
 cuadroID.grid(row = 0, column = 1, padx = 10, pady = 10)
 
-cuadroNombre = Entry(miFrame)
+miNombre = StringVar()
+
+cuadroNombre = Entry(miFrame, textvariable = miNombre)
 cuadroNombre.grid(row = 1, column = 1, padx = 10, pady = 10)
 cuadroNombre.config(fg = "red", justify = "right")
 
-cuadroPass = Entry(miFrame)
+miPassword = StringVar()
+
+cuadroPass = Entry(miFrame, textvariable = miPassword)
 cuadroPass.grid(row = 2, column = 1, padx = 10, pady = 10)
 cuadroPass.config(show = "*")
 
-cuadroApellido = Entry(miFrame)
+miApellido = StringVar()
+
+cuadroApellido = Entry(miFrame, textvariable = miApellido)
 cuadroApellido.grid(row = 3, column = 1, padx = 10, pady = 10)
 
-cuadroDireccion = Entry(miFrame)
+miDireccion = StringVar()
+
+cuadroDireccion = Entry(miFrame, textvariable = miDireccion)
 cuadroDireccion.grid(row = 4, column = 1, padx = 10, pady = 10)
 
 textoComentario = Text(miFrame, width = 14, height = 5)
@@ -81,7 +170,17 @@ labelComentarios.grid(row = 5, column = 0, sticky = "e", padx = 10, pady = 10)
 
 # ------------ Frame 2 ------------ 
 
+frame2 = Frame(root)
+frame2.pack()
 
+botonCrear = Button(frame2, text = "Create", command = crear)
+botonCrear.grid(row = 0, column = 0, padx = 10, pady = 10)
+botonLeer = Button(frame2, text = "Read", command = leer)
+botonLeer.grid(row = 0, column = 1, padx = 10, pady = 10)
+botonUpdate = Button(frame2, text = "Update", command = actualizar)
+botonUpdate.grid(row = 0, column = 2, padx = 10, pady = 10)
+botonDelete = Button(frame2, text = "Delete", command = eliminar)
+botonDelete.grid(row = 0, column = 3, padx = 10, pady = 10)
 
 
 
