83420170942c485888f8ae2ba821d6394f8a390f
Chino <matiastu-te_@hotmail.com>
2020-08-07 17:59:17 -0300

    .

--- prueba.py
+++ prueba.py
@@ -1,7 +1,3 @@
 import random
-#lista = [random.randint(0,5) for x in range(5)]
-lista = []
-for x in range(5):
-    lista.append(random.randint(0,5))
-
+lista = [random.randint(0,5) for x in range(5)]
 print(lista)
